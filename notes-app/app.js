73c496280846ca3e1405ea50d666ebddbd6b6a47
Stefen Hasselberg <stefen74@gmail.com>
2020-11-19 22:38:41 +1300

    clean up code

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,11 +1,4 @@
-// const fs = require("fs");
-const add = require("./utlis");
 const notesFn = require("./notes");
 
-// // fs.writeFileSync("notes.txt", "This file was created by Node.js");
-
-// fs.appendFileSync("notes.txt", "\nThis is a new line that is appended");
-
-// console.log(add(5, 8));
 const notes = notesFn();
 console.log(notes);
--- notes-app/utlis.js
+++
@@ -1,9 +0,0 @@
-console.log("utils.js");
-
-const name = "Mike";
-
-const add = function (x, y) {
-  return x + y;
-};
-
-module.exports = add;
