b609d614dee7f605a36db5726be446dc49891f45
Stefen Hasselberg <stefen74@gmail.com>
2020-11-23 22:02:50 +1300

    using yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,45 @@
-const chalk = require('chalk');
-const notesFn = require('./notes');
+const chalk = require("chalk");
+const yargs = require("yargs");
 
-const command = process.argv[2];
+const notesFn = require("./notes");
 
-if (command === 'add') {
-  console.log('adding note');
-} else if (command === 'remove') {
-  console.log('remove note');
-}
+// add, remove, read, list
+
+// create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: function () {
+    console.log("adding a new note");
+  },
+});
+
+// create remove command
+yargs.command({
+  command: "remove",
+  describe: "remove a note",
+  handler: function () {
+    console.log("remove note");
+  },
+});
+
+// create list command
+yargs.command({
+  command: "list",
+  describe: "list all notes",
+  handler: function () {
+    console.log("list all notes");
+  },
+});
+
+yargs.command({
+  command: "read",
+  describe: "read a note",
+  handler: function () {
+    console.log("read a note");
+  },
+});
+
+// create read command
+
+console.log(yargs.argv);
